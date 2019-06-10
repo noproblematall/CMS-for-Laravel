b2250f58ab3d2765246ca43d9081f9ecadbea80f
Luis Roy <luisroyrsg@gmail.com>
2019-06-10 23:42:54 +0800

    second commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <title>{{ config('app.name', 'CMS') }}</title>
 
     <!-- Scripts -->
-    {{-- <script src="{{asset('js/jquery.min.js')}}" defer></script> --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{asset('js/jquery.min.js')}}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
     <script src="{{ asset('js/sweetalert.min.js') }}" defer></script>
     <script src="{{ asset('js/main.js') }}" defer></script>
     
